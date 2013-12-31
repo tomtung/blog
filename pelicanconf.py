7fc2f49736a3afbf1ddb972c875e12b66623afe1
Tom Yubing Dong <tom.tung.dyb@gmail.com>
2013-12-31 11:08:42 +0800

    Change summary length and pagitation settings

--- pelicanconf.py
+++ pelicanconf.py
@@ -13,7 +13,8 @@ FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 
-DEFAULT_PAGINATION = 10
+SUMMARY_MAX_LENGTH = 200
+DEFAULT_PAGINATION = 5
 
 # Uncomment following line if you want document-relative URLs when developing
 RELATIVE_URLS = True
